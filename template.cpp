544ccbd6cf870300465a9317606aaedc85456fe6
Nick <jiseok95@gmail.com>
2019-12-19 23:44:10 +0900

    Add: template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int current_case = 0;
+    int case_count;
+    cin >> case_count;
+    vector<int> outputs;
+
+    while (current_case++ < case_count) {
+        int N;
+        cin >> N;
+    }
+    for (int output : outputs) cout << output << '\n';
+    return 0;
+}
